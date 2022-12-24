be5b90660cac28f2884b8e0ecf66cba1507de7e8
“sorry510” <sorry510sf@gmail.com>
2022-12-24 15:08:48 +0800

    feat: move kernel to framework

--- app/Simplex/Framework.php
+++ app/Simplex/Framework.php
@@ -4,23 +4,40 @@ namespace App\Simplex;
 
 use Symfony\Component\HttpKernel\HttpKernel;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\EventListener\RouterListener;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\HttpKernel\EventListener\ResponseListener;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolverInterface;
 use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
 
 class Framework extends HttpKernel implements HttpKernelInterface
 {
-    // public function __construct(
-    //     protected EventDispatcherInterface $dispatcher,
-    //     protected UrlMatcherInterface $urlMatcher,
-    //     protected ControllerResolverInterface $controllerResolver,
-    //     protected ArgumentResolverInterface $argumentResolver
-    // ){}
+    public function __construct($routes)
+    {
+        $context = new RequestContext();
+        $matcher = new UrlMatcher($routes, $context);
+        $requestStack = new RequestStack();
+
+        $controllerResolver = new ControllerResolver();
+        $argumentResolver = new ArgumentResolver();
+
+        $dispatcher = new EventDispatcher();
+        $dispatcher->addSubscriber(new RouterListener($matcher, $requestStack));
+        $dispatcher->addSubscriber(new ResponseListener('UTF-8'));
+        $dispatcher->addSubscriber(new StringResponseListener());
+
+        parent::__construct($dispatcher, $controllerResolver, $requestStack, $argumentResolver);
+    }
 
     // public function handle(Request $request, int $type = HttpKernelInterface::MAIN_REQUEST, $catch = true): Response
     // {
--- public/index.php
+++ public/index.php
@@ -3,66 +3,22 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use App\Simplex\Framework;
-use App\Simplex\GoogleListener;
-use Symfony\Component\Routing\Route;
-use App\Simplex\ContentLengthListener;
-use App\Simplex\StringResponseListener;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\HttpKernel\HttpCache\Esi;
 use Symfony\Component\HttpKernel\HttpCache\Store;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpKernel\HttpCache\HttpCache;
-use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Component\Routing\Matcher\CompiledUrlMatcher;
-use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
-use Symfony\Component\HttpKernel\EventListener\ErrorListener;
-use Symfony\Component\ErrorHandler\Exception\FlattenException;
-use Symfony\Component\HttpKernel\EventListener\RouterListener;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\Routing\Matcher\Dumper\CompiledUrlMatcherDumper;
-
-$request = Request::createFromGlobals();
-$requestStack = new RequestStack();
-
-$routes = require __DIR__ . '/../app/routers/web.php';
-
-$controllerResolver = new ControllerResolver();
-$argumentResolver = new ArgumentResolver();
-$matcher = new UrlMatcher($routes, new RequestContext);
-// $matcher = new CompiledUrlMatcher((new CompiledUrlMatcherDumper($routes))->getCompiledRoutes(), $context);
-
-$dispatcher = new EventDispatcher();
-// $dispatcher->addListener('response', [new GoogleListener, 'onResponse']);
-// $dispatcher->addListener('response', [new ContentLengthListener, 'onResponse'], -255); // 数字越大，优先级越高，默认为0
-
-// $dispatcher->addSubscriber(new ContentLengthListener());
-// $dispatcher->addSubscriber(new GoogleListener());
-
-$dispatcher->addSubscriber(new RouterListener($matcher, $requestStack));
-$dispatcher->addSubscriber(new ErrorListener(function (FlattenException $exception) {
-    $msg = 'Something went wrong! ('.$exception->getMessage().')';
-    return new Response($msg, $exception->getStatusCode());
-}));
-
-$dispatcher->addSubscriber(new StringResponseListener());
-
-// $listener = new ErrorListener(
-//     'Calendar\Controller\ErrorController::exception' // 自定义类+方法
-// );
-// $dispatcher->addSubscriber($listener);
 
 try {
-    $app = new Framework($dispatcher, $controllerResolver, $requestStack, $argumentResolver);
+    $request = Request::createFromGlobals();    
+    $routes = require __DIR__ . '/../app/routers/web.php';
+    $app = new Framework($routes);
     $app = new HttpCache(
         $app, 
         new Store(__DIR__ . '/../cache'),
         new Esi(),
-        ['debug' => true]
+        ['debug' => false]
     );
     $response = $app->handle($request);
 } catch (ResourceNotFoundException $exception) {
